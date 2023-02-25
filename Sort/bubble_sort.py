4763bd4689fd1314a1cd89dfdf0ada0915a70ea9
Duong Trung Kien <96287101+codingwithkien@users.noreply.github.com>
2023-02-25 22:56:46 +0700

    Update bubble_sort.py

--- Sort/bubble_sort.py
+++ Sort/bubble_sort.py
@@ -1,1 +1,13 @@
+def BubbleSort(array, n):
+  for i in range(0, n - 1):
+        for j in range(0, n - 1 - i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
 
+array = [5.5, 5.8, 4.6, 2.8]
+n = len(array)
+BubbleSort(array, n)
+
+print("Sorted array is:")
+for i in range(n):
+    print(array[i], end = "\t")
